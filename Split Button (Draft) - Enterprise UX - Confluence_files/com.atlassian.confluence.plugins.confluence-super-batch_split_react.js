54f9291a86e6eab2879bd5c5f35b75a1017af5fa
ybersofsky8484 <ybersofsky@gmail.com>
2025-11-11 16:28:13 +0000

    Remove unwanted files

--- Split Button (Draft) - Enterprise UX - Confluence_files/com.atlassian.confluence.plugins.confluence-super-batch_split_react.js
+++
@@ -1,6 +0,0 @@
-WRMCB=function(e){var c=console;if(c&&c.log&&c.error){c.log('Error running batched script.');c.error(e);}}
-;
-try {
-/* module-key = 'com.atlassian.confluence.plugins.confluence-super-batch:split_react', location = 'react.js' */
-(window.atlassianWebpackJsonp72e8e6db7d5e073657900987d0037a3a=window.atlassianWebpackJsonp72e8e6db7d5e073657900987d0037a3a||[]).push([[10],{149:function(a,e,d){"use strict";d.r(e);var n=d(20),s=d.n(n),t=d(2);Object(t.a)("react",()=>({default:s.a,...n}))}},[[149,0,2,4,1]]]);
-}catch(e){WRMCB(e)};
